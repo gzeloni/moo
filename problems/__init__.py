f0cca6a90e4d29aaea22eb5a50fb5006518e638f
gzeloni <gustavoluizdejesus@gmail.com>
2025-07-25 01:07:56 -0300

    ADD problems folder

---
+++ problems/__init__.py
@@ -0,0 +1,12 @@
+from problems.rcm06 import RCM06
+from problems.rcm13 import RCM13
+from problems.rcm28 import RCM28
+from problems.rcm29 import RCM29
+
+
+__all__ = [
+    "RCM06",
+    "RCM13",
+    "RCM28",
+    "RCM29",
+]
